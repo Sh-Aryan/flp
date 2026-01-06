f18fce93bb07b685aa131b5cb96545696658e0fe
killa-spotify <pranjaldeepansh@gmail.com>
2026-01-06 12:33:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.header("this is a header", divider="rainbow")
+st.subheader("this is a subheader")
+
+st.markdown('''
+    i am working on my laptop 
+    with creating my first website!
+    -1: happy
+    -2: 1st ever i am making
+    -2: wow
+    
+    this text is **bold**
+    [flipkart best find for you!!](https://www.flipkart.com/narachi-texworld-women-embroidered-a-line-kurta/p/itm7d0d732231235?pid=KTAHG7VGNAEAC9YQ&lid=LSTKTAHG7VGNAEAC9YQRLKEYG&marketplace=FLIPKART&store=clo%2Fcfv%2Fcib%2Frkt&srno=b_1_4&otracker=browse&fm=organic&iid=c6e02d24-7214-462c-8bb3-501717c7618a.KTAHG7VGNAEAC9YQ.SEARCH&ppt=browse&ppn=browse&ssid=ilsordufhc0000001767682832246)
+''')
